064a469eaf2b3b14e4b34aba56d2f6d99986a11a
Rin <a-efremova.m@yandex.ru>
2025-05-13 06:06:51 +0300

    откорректирована работа StatisticService

--- MovieQuiz/Services/StatisticService.swift
+++ MovieQuiz/Services/StatisticService.swift
@@ -2,33 +2,63 @@ import Foundation
 
 final class StatisticService: StatisticServiceProtocol {
     
+    private let storage: UserDefaults = .standard
+    
+    // MARK: - Keys
+    enum Keys: String {
+        case correct, gamesCount, total, date, totalCorrectAnswers
+    }
+    
+    // MARK: - Storage Properties
+    
     var gamesCount: Int {
-        return storedGamesCount
+        get {
+            storage.integer(forKey: Keys.gamesCount.rawValue)
+        }
+        set {
+            storage.set(newValue, forKey: Keys.gamesCount.rawValue)
+        }
     }
     
     var bestGame: GameResult {
-        return storedBestGame
+        get {
+            let correct = storage.integer(forKey: Keys.correct.rawValue)
+            let total = storage.integer(forKey: Keys.total.rawValue)
+            let date = storage.object(forKey: Keys.date.rawValue) as? Date ?? Date()
+            
+            return GameResult(correct: correct, total: total, date: date)
+        }
+        set {
+            storage.set(newValue.correct, forKey: Keys.correct.rawValue)
+            storage.set(newValue.total, forKey: Keys.total.rawValue)
+            storage.set(newValue.date, forKey: Keys.date.rawValue)
+        }
     }
     
-    private var correctAnswers: Int {
-        return storedCorrectAnswers
+    private var totalCorrectAnswers: Int {
+        get {
+            storage.integer(forKey: Keys.totalCorrectAnswers.rawValue)
+        }
+        set {
+            storage.set(newValue, forKey: Keys.totalCorrectAnswers.rawValue)
+        }
     }
     
     var totalAccuracy: Double {
         let totalQuestions = gamesCount * 10
         guard totalQuestions > 0 else { return 0 }
         
-        return (Double(correctAnswers)  / Double(totalQuestions)) * 100
+        return (Double(totalCorrectAnswers) / Double(totalQuestions)) * 100
         
     }
     
     func store(correct count: Int, total amount: Int) {
-        storedGamesCount += 1
-        storedCorrectAnswers += count
+        gamesCount += 1
+        totalCorrectAnswers += count
         
-        let currentGame = GameResult(correct: correctAnswers, total: amount, date: Date())
-        if currentGame.isBetterThan((storedBestGame)) {
-            storedBestGame = currentGame
+        let currentGame = GameResult(correct: count, total: amount, date: Date())
+        if currentGame.isBetterThan(bestGame) {
+            bestGame = currentGame
         }
     }
 }
